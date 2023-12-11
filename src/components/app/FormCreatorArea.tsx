b63f7fabd9bef999591dd0e464ee5f713ef7deb0
Filip Kniaś <filipknias@MacBook-Air-Filip.local>
2023-12-11 21:01:16 +0100

    show creator area message when dragging item

--- src/components/app/FormCreatorArea.tsx
+++ src/components/app/FormCreatorArea.tsx
@@ -7,12 +7,17 @@ import { Types } from "@/types/DndTypes";
 
 export default function FormCreatorArea() {
     const { formElements } = useFormCreator();
-    const { setNodeRef } = useDroppable({
+    const { setNodeRef, active } = useDroppable({
         id: Types.CREATOR_AREA,
     });
 
     return (
-        <Card className="h-full p-4 overflow-y-auto" ref={setNodeRef}>
+        <Card className="h-full p-4 overflow-y-auto relative" ref={setNodeRef}>
+            {active && (
+                <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center z-10">
+                    <h1 className="text-slate-200 text-4xl">Drop element here</h1>
+                </div>
+            )}
             <div className="flex flex-col gap-4">
                 {formElements.map(({ id, type }) => (
                     <ElementComponentOverlay id={id} key={id}>
--- src/components/app/FormElementTile.tsx
+++ src/components/app/FormElementTile.tsx
@@ -21,15 +21,20 @@ export default function FormElementTile({ formElement }: Props) {
         data: { type, attributes, indexPosition: elementsCount },
     });
 
+    const styles = {
+        transform: CSS.Translate.toString(transform),
+        zIndex: '50'
+    };
+
     return (
         <div
             ref={setNodeRef}
-            style={{ transform: CSS.Translate.toString(transform) }}
+            style={styles}
             {...draggableAttributes}
             {...listeners}
         >
             <div 
-                className={`rounded-md border border-slate-800 p-4 h-24 w-24 hover:border-slate-400 transition duration-200 ${isDragging ? "cursor-grab" : "cursor-grabbing"}`}
+                className={`rounded-md border border-slate-800 bg-slate-950 p-4 h-24 w-24 hover:border-slate-400 transition duration-200 ${isDragging ? "cursor-grab" : "cursor-grabbing"}`}
             >
                 <div className="flex justify-center mb-2 text-3xl"><IconElement /></div>
                 <p className="text-white text-sm font-medium text-center">{label}</p>
